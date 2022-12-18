24e41f83348bec3afa9307bdd5bf5fc1d8d06f4d
Hassan Adawie <adawiehhassan@gmail.com>
2022-12-18 12:04:15 +0200

    Added my name to the app!

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
         <SayHi />
         <SayHello color="black" name={this.state.name} />
         <Search handleInput={this.handleInputChange} />
+        <h1>Hassan</h1>
       </div>
     );
   }
